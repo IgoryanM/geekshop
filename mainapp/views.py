b735c331a4e1edd871631648a51bc6fd471fcf6e
Igor <igor.kerro@mail.ru>
2021-03-03 23:38:15 +0300

    HW_2

--- mainapp/views.py
+++ mainapp/views.py
@@ -2,12 +2,29 @@ from django.shortcuts import render
 
 
 def main(request):
-    return render(request, 'mainapp/index.html')
+    content = {
+        'title': 'main'
+    }
+    return render(request, 'mainapp/index.html', content)
 
 
 def products(request):
-    return render(request, 'mainapp/products.html')
+    links_menu = [
+        {'href': 'products_all', 'name': 'все'},
+        {'href': 'products_home', 'name': 'дом'},
+        {'href': 'products_office', 'name': 'офис'},
+        {'href': 'products_modern', 'name': 'модерн'},
+        {'href': 'products_classic', 'name': 'классика'},
+    ]
+    content = {
+        'title': 'products',
+        'links_menu': links_menu
+    }
+    return render(request, 'mainapp/products.html', content)
 
 
 def contact(request):
-    return render(request, 'mainapp/contact.html')
+    content = {
+        'title': 'contact'
+    }
+    return render(request, 'mainapp/contact.html', content)
