5b5620c6726fd7dbcec59d6297e2afadf91ceac8
Igor <igor.kerro@mail.ru>
2021-03-21 00:52:55 +0300

    iii

--- mainapp/views.py
+++ mainapp/views.py
@@ -1,4 +1,6 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+
+from basketapp.models import Basket
 from .models import ProductCategory, Product
 
 
@@ -12,12 +14,35 @@ def main(request):
 
 
 def products(request, pk=None):
-    same_products = Product.objects.all()
+    basket = 0
+    if request.user.is_authenticated:
+        basket = sum(list(Basket.objects.filter(user=request.user).values_list('quantity', flat=True)))
+
     links_menu = ProductCategory.objects.all()
+
+    if pk is not None:
+        if pk == 0:
+            products_list = Product.objects.all().order_by('price')
+            category_item = {}
+        else:
+            category_item = get_object_or_404(ProductCategory, pk=pk)
+            products_list = Product.objects.filter(category=category_item)
+        content = {
+            'title': 'products',
+            'links_menu': links_menu,
+            'category_item': category_item,
+            'products': products_list,
+            'basket': basket
+        }
+        return render(request, 'mainapp/products_list.html', content)
+
+    same_products = Product.objects.all()
+
     content = {
         'title': 'products',
         'links_menu': links_menu,
-        'same_products': same_products
+        'same_products': same_products,
+        'basket': basket
     }
     return render(request, 'mainapp/products.html', content)
 
