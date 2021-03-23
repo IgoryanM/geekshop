85690a0b99cdfc3f0332f26197d6183680791909
Igor <igor.kerro@mail.ru>
2021-03-23 14:54:40 +0300

    hw_6

--- mainapp/views.py
+++ mainapp/views.py
@@ -1,23 +1,38 @@
+import random
+
 from django.shortcuts import render, get_object_or_404
 
 from basketapp.models import Basket
 from .models import ProductCategory, Product
 
 
+def get_basket(user):
+    if user.is_authenticated:
+        return Basket.objects.filter(user=user)
+    return []
+
+
+def get_hot_product():
+    products_list = Product.objects.all()
+    return random.sample(list(products_list), 1)[0]
+
+
+def get_same_products(hot_product):
+    same_products = Product.objects.filter(category=hot_product.category).exclude(pk=hot_product.pk)[:3]
+    return same_products
+
+
 def main(request):
     products = Product.objects.all()[:4]
     content = {
         'title': 'main',
-        'products': products
+        'products': products,
+        'basket': get_basket(request.user)
     }
     return render(request, 'mainapp/index.html', content)
 
 
 def products(request, pk=None):
-    basket = 0
-    if request.user.is_authenticated:
-        basket = sum(list(Basket.objects.filter(user=request.user).values_list('quantity', flat=True)))
-
     links_menu = ProductCategory.objects.all()
 
     if pk is not None:
@@ -32,23 +47,37 @@ def products(request, pk=None):
             'links_menu': links_menu,
             'category_item': category_item,
             'products': products_list,
-            'basket': basket
+            'basket': get_basket(request.user)
         }
+
         return render(request, 'mainapp/products_list.html', content)
 
-    same_products = Product.objects.all()
+    hot_product = get_hot_product()
+    same_products = get_same_products(hot_product)
 
     content = {
         'title': 'products',
         'links_menu': links_menu,
+        'hot_product': hot_product,
         'same_products': same_products,
-        'basket': basket
+        'basket': get_basket(request.user)
     }
     return render(request, 'mainapp/products.html', content)
 
 
+def product(request, pk):
+    content = {
+        'title': 'продукт',
+        'product': get_object_or_404(Product, pk=pk),
+        'links_menu': ProductCategory.objects.all(),
+        'basket': get_basket(request.user)
+    }
+    return render(request, 'mainapp/product.html', content)
+
+
 def contact(request):
     content = {
-        'title': 'contact'
+        'title': 'contact',
+        'basket': get_basket(request.user)
     }
     return render(request, 'mainapp/contact.html', content)
