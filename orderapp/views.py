24c04313a6eb4678d71a584b1484954734725e30
Igor <igor.kerro@mail.ru>
2021-04-21 22:42:39 +0300

    hw_2_4

--- orderapp/views.py
+++ orderapp/views.py
@@ -2,12 +2,13 @@ from django.db import transaction
 from django.db.models.signals import pre_save, pre_delete
 from django.dispatch import receiver
 from django.forms import inlineformset_factory
-from django.http import HttpResponseRedirect
+from django.http import HttpResponseRedirect, JsonResponse
 from django.shortcuts import render, get_object_or_404
 from django.urls import reverse_lazy, reverse
 from django.views.generic import ListView, CreateView, DetailView, UpdateView, DeleteView
 
 from basketapp.models import Basket
+from mainapp.models import Product
 from orderapp.forms import OrderForm, OrderItemForm
 from orderapp.models import Order, OrderItem
 
@@ -154,3 +155,12 @@ def product_quantity_update_save(sender, update_fields, instance, **kwargs):
 def product_quantity_update_delete(sender, instance, **kwargs):
     instance.product.quantity += instance.quantity
     instance.product.save()
+
+
+def get_product_price(request, pk):
+    if request.is_ajax():
+        product = Product.objects.filter(pk=int(pk)).first()
+        if product:
+            return JsonResponse({'price': product.price})
+        else:
+            return JsonResponse({'price': product.price})
