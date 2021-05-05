ddcf046531478f556bd548f3284899037f458fb3
Igor <igor.kerro@mail.ru>
2021-05-05 07:26:13 +0300

    hw8

--- basketapp/views.py
+++ basketapp/views.py
@@ -1,4 +1,5 @@
 from django.contrib.auth.decorators import login_required
+from django.db.models import F
 from django.http import HttpResponseRedirect, JsonResponse
 from django.shortcuts import get_object_or_404, render
 from django.template.loader import render_to_string
@@ -30,7 +31,7 @@ def basket_add(request, pk):
     if not basket_item:
         basket_item = Basket(user=request.user, product=product_item)
 
-    basket_item.quantity += 1
+    basket_item.quantity = F('quantity') + 1
     basket_item.save()
 
     return HttpResponseRedirect(request.META.get('HTTP_REFERER'))
